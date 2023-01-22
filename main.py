878b2fd6eea42ca7bbd05b673ee74dde71279fa3
Kyu Lee <kwooler@gmail.com>
2023-01-22 10:54:27 -0800

    L79

--- main.py
+++ main.py
@@ -17,24 +17,6 @@ def main():
 
     names = input('Enter 5 students names').split()
 
-    for i in range(len(names)):
-        if i == 0 or minlen > len(names[i]):
-            minlen = len(names[i])
-            minidx = i
-        elif minlen == len(names[i]) and names[i] < names[minidx]:
-            minlen = len(names[i])
-            minidx = i
-
-        if i == 0 or maxlen < len(names[i]):
-            maxlen = len(names[i])
-            maxidx = i
-        elif maxlen == len(names[i]) and names[i] < names[maxidx]:
-            maxlen = len(names[i])
-            maxidx = i
-
-    main.shortest = names[minidx]
-    main.longest = names[maxidx]
-
     print('The shortest name is ', main.shortest)
     print('The longest name is ', main.longest)
 ##
